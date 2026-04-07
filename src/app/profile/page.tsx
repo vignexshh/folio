9073e6e6db68c451e117f98af5bf75b42c7e0426
Vignesh T D <vigneshtd@outlook.com>
2026-04-07 17:15:09 +0530

    static audio [sfx]

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -16,18 +16,47 @@ import { Button } from "@/components/ui/button";
 import Portfolio from "@/components/portfolio/RenderPortfio";
 import { staticRadioNoiseLoop } from "@/components/ui-sounds/SoundManager";
 
+import { GoMute } from "react-icons/go";
+import { GoUnmute } from "react-icons/go";
+
+
 export default function PortfolioPage() {
     const [visitIntent, setVisitIntent] = useState(false);
+    const [staticAudioStatus, SetstaticAudioStatus] = useState(false);
+    // const handleStaticAudio = () => {
+
+    // }
     const handleIntent = () => {
         setVisitIntent(true)
         staticRadioNoiseLoop.play()
 
     }
+    const handleAudio = () => {
+        if (staticRadioNoiseLoop.playing()) {
+            staticRadioNoiseLoop.stop();
+            SetstaticAudioStatus(false)
+
+        } else {
+            staticRadioNoiseLoop.play();
+            SetstaticAudioStatus(true)
+        }
+    }
 
     return (
 
         <>
-            {visitIntent ? <Portfolio /> : (
+            {visitIntent ? <>
+                <Portfolio />
+                <div className="fixed bottom-6 right-6 z-50">
+                    <Button
+                        size="icon"
+                        variant="outline" aria-label="Submit"
+                        onClick={handleAudio}
+                    >
+                    {staticAudioStatus ? (<GoUnmute/>) : (<GoMute/>)}
+                    </Button>
+                </div>
+            </> : (
 
                 <div className="h-screen flex items-center justify-center bg-black text-white">
                     <Button onClick={handleIntent} className="px-6 py-3 bg-white text-black">
